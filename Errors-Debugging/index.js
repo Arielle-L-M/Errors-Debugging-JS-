d6cbbf734eef7309209ae3fa54ae023774c6ff13
Arielle Moon <amoon@icstars.org>
2026-02-23 15:11:29 -0600

    Attempting a try catch statement:(

--- Errors-Debugging/index.js
+++ Errors-Debugging/index.js
@@ -22,8 +22,14 @@ function findMax(numbers) {
  return total;
 }
 
-const data1 = [1, 2, 3];
-const data2 = [4, 5, 6];
+const data3 = [1, 2, 3];
+const data4 = [4, 5, 6];
 
-console.log(calculateTotal(data1));
-console.log(calculateTotal(data2));
+console.log(calculateTotal(data3));
+console.log(calculateTotal(data4));
+
+try {                                   /*Not understanding how to produce try/catch statements- I understand their purpose, but replicating them to produce my own isn't clicking:(*/
+  if(console.log(>=numbers.length))
+} catch (error) {
+  "Number too high"
+}
